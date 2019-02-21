a2def3c9aba8aa58fbdf1766728fb17c28c772c8
Ben Bradford <bbradford@wisc.edu>
2019-02-21 13:15:51 -0600

    Added section for generating a PVY-transmission aggregate risk score for aphids, and included code for generating phenology curves from this aggregate risk.

--- pvy.R
+++ pvy.R
@@ -1,16 +1,19 @@
+### PVY-relevant aphid species and infection risk ###
+
+# Load packages ----
+library(tidyverse)
 
 
-# read pvy infectivity data
-pvy_inf <-
-  read.csv("data/pvy-inf.csv", header = TRUE)
-names(pvy_inf) <- c("SpeciesName", "InfLow", "InfUp", "InfAvg")
+# Read pvy infectivity data ----
+
+pvy_inf = read.csv("data/pvy-inf.csv", header = TRUE)
+names(pvy_inf) = c("SpeciesName", "InfLo", "InfHi", "InfAvg")
 
 
-
-# Filters -----------------------------------------------------------------
+# Subsets of the aphid dataset ----
 
 # all potential pvy vectors
-aphid <-
+aphid_pvy =
   aphid_full %>%
   filter(
     SpeciesName %in% c(
@@ -33,10 +36,9 @@ aphid <-
   droplevels()
 
 
-# PVY species with known infectivity
-
 # keep aphid species with PVY risk values
-aphid_pvy <- aphid_full %>%
+aphid_pvy = 
+  aphid_full %>%
   left_join(pvy_inf) %>%
   mutate(SpeciesName = as.factor(SpeciesName)) %>%
   drop_na(InfAvg) %>%
@@ -44,33 +46,30 @@ aphid_pvy <- aphid_full %>%
          PVYRisk = log(Count + 1)*asin(sqrt(InfAvg))) %>%
   droplevels()
 
-# keep wisconsin only
-aphid_pvy <- filter(aphid_pvy, State == "WI")
 
 # collapse to aggregate counts
-aphid_pvy_aggr <-
+aphid_pvy_aggr =
   aphid_pvy %>%
   group_by(SampleID) %>%
   summarise(
+    State = State[1],
     SiteID = SiteID[1],
-    SpeciesName = "Aggregate",
     Year = Year[1],
     GDD39 = GDD39[1],
-    Count = sum(PVYRisk)
+    Risk = as.integer(sum(PVYRisk)*10)
   )
 
-# collapse to aggregate counts, intergers only
-aphid_pvy_aggr <-
+# collapse to aggregate counts, integers only
+aphid_pvy_aggr =
   aphid_pvy %>%
   group_by(SampleID) %>%
   summarise(
+    State = State[1],
     SiteID = SiteID[1],
-    SpeciesName = "Aggregate",
     Year = Year[1],
     GDD39 = GDD39[1],
     Count = as.integer(sum(PVYCount))
   )
-
 
 
 # Summaries ---------------------------------------------------------------
@@ -83,44 +82,155 @@ aphid_pvy %>%
   write.csv("out/wi_pvy_wkly.csv")
 
 
-# Phenology code ----------------------------------------------------------
+# Phenology code -----
+library(lme4)
+library(mgcv)
 
 # CMs from PVY Risk
-cm_gdd_pvycount =
+pvy_cm =
   aphid_pvy_aggr %>%
-  group_by(SpeciesName) %>%
-  do(remfn(.))
+  {
+    fmod =
+      ranef(glmer(
+        Risk ~ 1 + (1 | GDD39) + (1 | Year) + (1 | SiteID),
+        data = .,
+        family = "poisson"
+      ))
+    data.frame(GDD = as.numeric(rownames(fmod$GDD39)),
+               CM = fmod$GDD39[, 1])
+  }
 
-# generate predictions from gam fit
-gampreds = gampredfn(cm_gdd_pvycount)
+pvy_gampreds =
+  cbind(data.frame(GDD = 1:7500),
+        CM = predict.gam(gam(CM ~ s(GDD), data = pvy_cm),
+                    data.frame(GDD = 1:7500)))
 
-# identify critical points
-gampts = gamptfn(gampreds)
+pvy_gampts =
+  pvy_gampreds %>%
+  mutate(
+    type = case_when(
+      sign(lag(CM)) < sign(CM) ~ "RI",
+      sign(lag(CM)) > sign(CM) ~ "FI",
+      lag(CM) < CM & CM > lead(CM) ~ "max",
+      lag(CM) > CM & CM < lead(CM) ~ "min"
+    )
+  ) %>%
+  na.omit()
+
+p =
+  pvy_cm %>%
+  ggplot(aes(x = GDD, y = CM)) +
+  scale_x_sqrt() +
+  stat_smooth(
+    method = "gam",
+    formula = y ~ s(x),
+    fill = "grey50",
+    colour = "Black",
+    size = 1.5
+  ) +
+  geom_abline(intercept = 0, slope = 0) +
+  labs(x = "GDDs",
+       y = "CMs (GDD39/86)",
+       title = paste("GDD39/86 fits for Suction Traps",
+                     min(aphid$Year), "-", max(aphid$Year))
+  ) +
+  theme(strip.text = element_text(size = 14, face = "bold")) +
+  geom_line(data = pvy_gampreds, color = "red", size = 1.5) +
+  geom_point(data = pvy_gampts, aes(x = GDD, y = CM)) +
+  geom_label(data = pvy_gampts, aes(x = GDD, y = CM, label = GDD))
+p
 
 
 
 
-# Subset into groups ------------------------------------------------------
+# test by state ----
+pvy_cm =
+  aphid_pvy_aggr %>%
+  group_by(State) %>%
+  do({
+    fmod =
+      ranef(glmer(
+        Risk ~ 1 + (1 | GDD39) + (1 | Year),
+        data = .,
+        family = "poisson"
+      ))
+    data.frame(GDD = as.numeric(rownames(fmod$GDD39)),
+               CM = fmod$GDD39[, 1])
+  })
+p =
+  pvy_cm %>%
+  ggplot(aes(x = GDD, y = CM)) +
+  geom_hline(yintercept = 0) +
+  stat_smooth(
+    method = "gam",
+    formula = y ~ s(x),
+    fill = "grey50",
+    colour = "Black",
+    size = 1.5
+  ) +
+  facet_wrap( ~ State, scales = "free") +
+  labs(title = "PVY Risk estimates by degree day (39/86)") +
+  theme(strip.text = element_text(face = "bold"))
+p
+CairoPNG("out/pvy_risk_cm.png", h = 800, w = 1200); p; dev.off()
 
-# subsets for phenology plots
-aphid1 <- aphid_full %>%
-  filter(SpeciesName %in% c(
-    "Aphis glycines",
-    "Rhopalosiphum padi",
-    "Acyrthosiphon pisum",
-    "Myzus persicae"
-  )) %>% droplevels()
+# predicted values from a smooth fit of CMs
+pvy_gampreds =
+  pvy_cm %>%
+  group_by(State) %>%
+  do(
+  cbind(data.frame(GDD = 1:7500),
+        CM = predict.gam(gam(CM ~ s(GDD), data = .),
+                         data.frame(GDD = 1:7500)))
+  )
 
-aphid2 <- aphid_full %>%
-  filter(SpeciesName %in% c(
-    "Rhopalosiphum maidis",
-    "Capitophorus elaeagni",
-    "Macrosiphum euphorbiae",
-    "Aphis craccivora"
-  )) %>% droplevels()
+# inflection points and intercepts
+pvy_gampts =
+  pvy_gampreds %>%
+  group_by(State) %>%
+  mutate(
+    type = case_when(
+      sign(lag(CM)) < sign(CM) ~ "RI",
+      sign(lag(CM)) > sign(CM) ~ "FI",
+      lag(CM) < CM & CM > lead(CM) ~ "max",
+      lag(CM) > CM & CM < lead(CM) ~ "min"
+    )
+  ) %>%
+  na.omit()
 
-aphid3 <- aphid_full %>%
-  filter(SpeciesName %in% c(
-    "Sitobion avenae",
-    "Brachycaudus helichrysi"
-  )) %>% droplevels()
+
+# graph
+pvy_gampreds %>%
+  group_by(State) %>%
+  ggplot(aes(x = GDD, y = CM)) +
+  geom_hline(yintercept = 0) +
+  geom_line(color = "red", size = 1) +
+  facet_wrap(~State) +
+  geom_point(data = pvy_gampts, aes(x = GDD, y = CM))
+
+
+
+# # Subset into groups ------------------------------------------------------
+# 
+# # subsets for phenology plots
+# aphid1 <- aphid_full %>%
+#   filter(SpeciesName %in% c(
+#     "Aphis glycines",
+#     "Rhopalosiphum padi",
+#     "Acyrthosiphon pisum",
+#     "Myzus persicae"
+#   )) %>% droplevels()
+# 
+# aphid2 <- aphid_full %>%
+#   filter(SpeciesName %in% c(
+#     "Rhopalosiphum maidis",
+#     "Capitophorus elaeagni",
+#     "Macrosiphum euphorbiae",
+#     "Aphis craccivora"
+#   )) %>% droplevels()
+# 
+# aphid3 <- aphid_full %>%
+#   filter(SpeciesName %in% c(
+#     "Sitobion avenae",
+#     "Brachycaudus helichrysi"
+#   )) %>% droplevels()
